f472d6436199f37da6a80eaca23220c5dcae74a5
Joakim Andén <janden@math.princeton.edu>
2017-02-10 11:37:55 -0500

    Add default number of bins for thist
    Same as the regular hist function: 10.

--- thist.m
+++ thist.m
@@ -7,7 +7,8 @@
 %    x: The values whose histogram is to be displayed.
 %    bins: The bins for the histogram. If this is an array, these are the bin
 %       centers that are used. If this is a scalar, it indicates the number of
-%       equispaced bins to use, with bin centers calculated automatically.
+%       equispaced bins to use, with bin centers calculated automatically
+%       (default 10).
 %
 % Description
 %     This function uses the standard hist function to calculate the histogram
@@ -15,6 +16,10 @@
 %     graphics window.
 
 function thist(x, bins)
+    if nargin < 2 || isempty(bins)
+        bins = 10;
+    end
+
     n = hist(x, bins);
 
     if size(n, 1) == 1
