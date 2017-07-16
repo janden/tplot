2a67585481b9e144722465d646d69bfebfd8f0d0
Joakim Andén <janden@math.princeton.edu>
2017-07-15 20:32:40 -0400

    Add 26-color rendering in `timagesc`
    Basic implementation using the 26 grayscale colors of the fixed
    256 palette.

--- timagesc.m
+++ timagesc.m
@@ -38,6 +38,46 @@ function timagesc_render(buf, render_mode)
             fprintf('%c', chars(k,:));
             fprintf('\n');
         end
+    elseif render_mode == 1
+        shade_ct = 26;
+
+        buf = min(floor(buf*shade_ct), shade_ct-1);
+
+        if mod(size(buf, 1), 2) == 1
+            buf = cat(1, buf, zeros(1, size(buf, 2)));
+        end
+
+        escape_str = [char(27) '['];
+        reset_str = '0';
+        fg_str = '38;5;%d';
+        bg_str = '48;5;%d';
+        join_str = ';';
+        unescape_str = 'm';
+
+        up_str = {};
+        down_str = {};
+
+        up_str{1} = sprintf(fg_str, 0);
+        down_str{1} = sprintf(bg_str, 0);
+        up_str{shade_ct} = sprintf(fg_str, 15);
+        down_str{shade_ct} = sprintf(bg_str, 15);
+
+        for k = 2:shade_ct-1
+            up_str{k} = sprintf(fg_str, 232+(k-1));
+            down_str{k} = sprintf(bg_str, 232+(k-1));
+        end
+
+        block_str = sprintf('%c', hex2dec('2580'));
+
+        for k = 1:2:size(buf, 1)
+            for l = 1:size(buf, 2)
+                fprintf([escape_str up_str{buf(k,l)+1} join_str ...
+                    down_str{buf(k+1,l)+1} unescape_str block_str]);
+            end
+            fprintf('\n');
+        end
+
+        fprintf([escape_str reset_str unescape_str]);
     else
         error('Invalid `render_mode`.');
     end
